5bc2b7d3bedc1f47ae80dc08ed88b11675ae7143
vasily <artur.dvo@mail.ru>
2020-09-23 11:24:43 +0300

    Added models adress, city, country, street, department. Added this models in the admin site.

--- funsite/admin.py
+++ funsite/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
-from funsite.models import Brand, News, Carousel, CompanyInfo
+from funsite.models import Brand, News, Carousel, CompanyInfo, Department, Address, Country, City, Street
 
 
 class BrandAdmin(admin.ModelAdmin):
@@ -42,7 +42,29 @@ class CompanyInfoAdmin(admin.ModelAdmin):
     list_display = ('title', 'description')
 
 
+class AddressInline(admin.TabularInline):
+    model = Address
+    extra = 1
+
+
+class DepartmentAdmin(admin.ModelAdmin):
+    """Displaying the info about department in the admin panel"""
+    inlines = [AddressInline, ]
+    list_display = ('name_department',)
+
+
+class AddressAdmin(admin.ModelAdmin):
+    """Displaying the info about street in the admin panel"""
+    list_display = ('title', 'department', 'country', 'city', 'street', 'number_house',
+                    'number_flat', 'number_office')
+
+
 admin.site.register(CompanyInfo, CompanyInfoAdmin)
 admin.site.register(Brand, BrandAdmin)
 admin.site.register(News, NewsAdmin)
 admin.site.register(Carousel, CarouselAdmin)
+admin.site.register(Department, DepartmentAdmin)
+admin.site.register(Address, AddressAdmin)
+admin.site.register(Country)
+admin.site.register(City)
+admin.site.register(Street)
--- funsite/models.py
+++ funsite/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from datetime import datetime
 from os.path import splitext
 from PIL import Image
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 
 def get_timestamp_path(instance, filename):
@@ -37,7 +38,8 @@ class News(models.Model):
     title = models.CharField(max_length=200, verbose_name='Название новости')
     text = models.TextField(verbose_name='Текст новости')
     image_news = models.ImageField(blank=True, upload_to=get_timestamp_path,
-                                   verbose_name='Загрузить изображение*')
+                                   verbose_name='Загрузить изображение* Изображение должно'
+                                                'быть квадратным для корректного отображения')
     created_date = models.DateTimeField(auto_now_add=True, db_index=True,
                                         verbose_name='Дата публикации')
 
@@ -87,10 +89,83 @@ class Carousel(models.Model):
 
 class CompanyInfo(models.Model):
     """Model with company information"""
-    title = models.CharField(max_length=255, verbose_name='Название Компании')
+    title = models.CharField(max_length=100, verbose_name='Название Компании')
     description = models.TextField(verbose_name='Описание компании')
 
     class Meta:
-        """Attribute allows you to use the plural form of 'News'"""
         verbose_name = 'Инфо о компании'
         verbose_name_plural = 'Инфо о компании'
+
+
+class Department(models.Model):
+    """The model describes the department of the company."""
+    name_department = models.CharField(max_length=50, verbose_name='Название подразделения',
+                                       help_text='Например, Офис')
+    working_hours = models.CharField(max_length=30, verbose_name='Часы работы',
+                                     help_text='Пример: С 9-00 до 18-00')
+
+    def __str__(self):
+        return self.name_department
+
+    class Meta:
+        verbose_name = 'Подразделение'
+        verbose_name_plural = 'Подразделения'
+
+
+class Address(models.Model):
+    """The model describes the address"""
+    title = models.CharField(max_length=50, blank=True, verbose_name='Описание адреса',
+                             help_text='Например, Адрес склада')
+    department = models.ForeignKey('Department', on_delete=models.CASCADE, verbose_name='Название подразделения')
+    country = models.ForeignKey('Country', on_delete=models.CASCADE, verbose_name='Страна')
+    city = models.ForeignKey('City', on_delete=models.CASCADE, verbose_name='Город')
+    street = models.ForeignKey('Street', on_delete=models.CASCADE, verbose_name='Улица')
+    number_house = models.PositiveSmallIntegerField(verbose_name='Номер дома', validators=[
+        MaxValueValidator(1000), MinValueValidator(1)])
+    number_flat = models.PositiveSmallIntegerField(verbose_name='Номер помещения', blank=True, validators=[
+        MaxValueValidator(1000), MinValueValidator(1)])
+    number_office = models.PositiveSmallIntegerField(verbose_name='Номер офиса', blank=True, validators=[
+        MaxValueValidator(1000), MinValueValidator(1)])
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Адрес'
+        verbose_name_plural = 'Адреса'
+
+
+class Country(models.Model):
+    """The model describes the country"""
+    name_country = models.CharField(max_length=56, verbose_name='Название страны')
+
+    def __str__(self):
+        return self.name_country
+
+    class Meta:
+        verbose_name = 'Страна'
+        verbose_name_plural = 'Страны'
+
+
+class City(models.Model):
+    """The model describes the city"""
+    name_city = models.CharField(max_length=56, verbose_name='Название города')
+
+    def __str__(self):
+        return self.name_city
+
+    class Meta:
+        verbose_name = 'Город'
+        verbose_name_plural = 'Города'
+
+
+class Street(models.Model):
+    """The model describes the street"""
+    name_street = models.CharField(max_length=56, verbose_name='Название улицы')
+
+    def __str__(self):
+        return self.name_street
+
+    class Meta:
+        verbose_name = 'Улица'
+        verbose_name_plural = 'улицы'
